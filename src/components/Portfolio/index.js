a85f31c89beb6ae4b0e759cfd9c891c91e244d5d
WebmasterShane <shanelwebber@gmail.com>
2021-04-01 20:17:28 -0700

    updated portfolio with web-dev projects

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -8,14 +8,29 @@ import img6 from "./images/matrix.jpg"
 import img7 from "./images/mj_dk_1.jpg"
 import img8 from "./images/ps_roof.jpg"
 import img9 from "./images/Starlogged.jpg"
+import Cryptic from "./images/Cryptic.png"
+import Planner from "./images/Planner.png"
+import Laughing from "./images/Laughing.png"
 import Grid from "@material-ui/core/Grid";
 import Container from "@material-ui/core/Container";
 import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
 
 const useStyles = makeStyles({
     container:{
         marginTop: "10%",
         paddingBottom: "10%"
+
+    },
+    githubBtn:{
+        color: "#ffffff",
+        width: "50%",
+        marginLeft: "-6.5%"
+
+    },
+   linkBtn:{
+        color: "#ffffff",
+        width: "50%",
 
     }
   
@@ -26,7 +41,30 @@ function Portfolio() {
     const classes = useStyles()
     return(
         <Container className={classes.container}>
+              
             <Grid justify="center" container spacing={3}>
+            <Grid  item xs={4}>
+                <img className="img-fluid" src={Cryptic} alt="Cryptic Home Page"></img>
+                <Grid row>
+                <Button variant="outlined"className={classes.githubBtn} color="secondary" href="https://github.com/AlignSD/Ticket_Bits">GitHub</Button>
+                <Button variant="outlined"className={classes.linkBtn} color="primary" href="https://ticket-bits.herokuapp.com/">Cryptic</Button>
+                </Grid>
+                </Grid>
+                <Grid item xs={4}>
+                <img className="img-fluid" src={Planner} alt="Planner for Planners"></img>
+                <Grid row>
+                <Button variant="outlined"className={classes.githubBtn} color="secondary" href="https://github.com/916Serg619/Project2">GitHub</Button>
+                <Button variant="outlined"className={classes.linkBtn} color="primary" href="https://salty-earth-89090.herokuapp.com/">P4P</Button>
+                </Grid>
+                </Grid>
+                <Grid item xs={4}>
+                <img className="img-fluid" src={Laughing} alt="Laughing Save the Day"></img>
+                <Grid row>
+                <Button variant="outlined"className={classes.githubBtn} color="secondary" href="https://github.com/larrygamboa/laughing-saves-the-day">GitHub</Button>
+                <Button variant="outlined"className={classes.linkBtn} color="primary" href="https://larrygamboa.github.io/laughing-saves-the-day/">LSD</Button>
+                </Grid>
+                </Grid>
+                
                 <Grid item xs={4}>
                 <img className="img-fluid" src={img1} alt="House2Ourselves logo"></img>
                 </Grid>
